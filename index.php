418e19e73fa37998e5726a63272e4e2e7d1d4442
Y.Kawai <macking.all@gmail.com>
2020-11-11 13:25:51 +0900

    あたりさわりなく

--- index.php
+++ index.php
@@ -1,7 +1,14 @@
 <?php
-phpinfo();
+$hw = 'Helow World!'
 ?>
-
-
-
-<!-- 鈴木　航也pushテスト -->
+<!DOCTYPE html>
+<html lang="ja_JP">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1><?=$hw?></h1>
+</body>
+</html>
