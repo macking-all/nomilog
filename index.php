1d187241006dbfe281bf538a6944bb09d04e8692
mori <takaaki.578578@gmail.com>
2020-11-13 00:04:23 +0900

    test

--- index.php
+++ index.php
@@ -13,8 +13,8 @@ $hw = 'Helow Sake World!';
     <dl>
         <dt>アクティビティ機能</dt>
         <dt>呑みたい人マッチング機能</dt>
-        <dt>aaaa</dt>
-        <dt>aaa</dt>
+        <dt></dt>
+        <dt></dt>
         <dt>鈴木　航也鈴木　航也鈴木　航也</dt>
         <dt></dt>
         <dt></dt>
