e65f49b2a6fb913dd5dcdb66d8956cce81a91656
tarogneer <ginyou26@gmail.com>
2020-11-17 21:27:08 +0900

    お試しがてらお店一覧ページたたき 台の作成

---
+++ board.php
@@ -0,0 +1,20 @@
+<?php
+    $recommend = "本日のおすすめ";
+?>
+<!DOCTYPE html>
+<html lang="ja_JP">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="top" href="index.php">
+    <title>お店一覧</title>
+    <!--
+    マイページへのリンク作成予定
+    <link rel="mypage" href = "">
+    -->
+</head>
+<body>
+    <li><?=$recommend?></li>
+    
+</body>
+</html>
