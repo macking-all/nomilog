1a7c06cfedf63bbd6239ce654c9ba556be33f7a2
Y.Kawai <macking.all@gmail.com>
2020-12-27 00:44:02 +0900

    いろいろ整理

--- source/nlfw/config/daoConfig.php
+++ source/nlfw/config/daoConfig.php
@@ -4,7 +4,9 @@ namespace nlfw\config;
 
 class daoConfig {
     public const SERVER_NAME = "nomilog_nldb_1";
-    public const DSN = "mysql:host=".self::SERVER_NAME.";dbname=nomilog;port=3306";
+    public const DB_NAME = "nomilog";
+    public const DB_PORT = "3306";
+    public const DSN = "mysql:host=".self::SERVER_NAME.";dbname=".self::DB_NAME.";port=".self::DB_PORT;
     public const DB_USER = "root";
     public const DB_PWD = "root";
 }
