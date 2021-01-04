07f4ba116a4b124160ab9d1cbee0339a67007f4f
Y.Kawai <macking.all@gmail.com>
2021-01-05 00:31:58 +0900

    いろいろ

--- source/nlfw/controls/htmlControls.php
+++ source/nlfw/controls/htmlControls.php
@@ -9,6 +9,11 @@ function create_input($type,$id,$class,$value,$list = null) {
     };
 }
 
+function load_parts($name) {
+    return file_get_contents("./{$name}");
+}
+
+
 function create_inputElement($type,$id,$class,$value) {
     $__ = function ($v) { return $v;};
     return "<input type=\"{$type}\" id=\"{$id}\" class=\"{$__(parse_class($class))}\" value=\"{$value}\" />";
