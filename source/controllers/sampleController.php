fae8aca689666361445362800bf6ad93c97a0275
Y.Kawai <macking.all@gmail.com>
2021-01-26 20:57:13 +0900

    池袋式動作

--- source/controllers/sampleController.php
+++ source/controllers/sampleController.php
@@ -1,20 +1,14 @@
 <?php
 namespace nomilog\controllers;
-require("baseController.php");
-use nomilog\controllers\baseController;
-class sampleController extends baseController {
+class sampleController {
     function __construct($r) {
-        error_log("sample __construct");
-        parent::__construct($r);
     }
 
     function postAction(){
-        $this->define("default")?->html("index.html");
-        $this->define("search")?->action("debug")->html("index.html");
     }
 
     function debug(){
-        var_dump($_SESSION["__CURRENT"]["ACTION"]);
+        
     }
 }
 ?>
--- source/public/sample/index.php
+++ source/public/sample/index.php
@@ -1,7 +1,19 @@
 <?php
+require("../../controllers/baseController.php");
 require("../../controllers/sampleController.php");
+use nomilog\controllers\baseController;
 use nomilog\controllers\sampleController;
 
-$instance = new sampleController($_REQUEST);
-$instance->postAction();
+$instance = new baseController($_REQUEST);
+
+/*
+処理置き場
+*/
+$instance->define("default")?->html("index.html");
+$instance->define("search")?->action("debug")->html("index.html");
+
+function debug(){
+    var_dump($_SESSION["__CURRENT"]["ACTION"]);
+}
+
 ?>
