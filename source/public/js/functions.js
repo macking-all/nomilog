5a5ae15c49597ff1ba7cd8200f641708cfbd5120
鈴木　航也 <kouyan9614@gmail.com>
2021-02-06 20:50:56 +0900

    0206_新規登録画面（仮）

--- source/public/js/functions.js
+++ source/public/js/functions.js
@@ -3,11 +3,48 @@ console.log(test);
 
 //clickの後は関数を指定しないといけない
 $('#submitbtn').click(function () {
-    //トリガーが必要
-    let valtest = $('input[name = "user-name"]').val();
 
-    if (valtest === '') {
-        console.log("あああああああああああ");
+    //user-nameが空の時の処理
+    let valtest1 = $('input[name = "user-name"]').val();
+
+    if (valtest1 === '') {
+        $('.input-error-border1').addClass("error-border-color");
+        $('#required-error-text1').text("未入力です");
+        return false;
+    } else {
+        console.log("いいいいいいいいいい");
+        // return true;
+    }
+
+    //email-nameが空の時の処理
+    let valtest2 = $('input[name = "email-name"]').val();
+
+    if (valtest2 === '') {
+        $('.input-error-border2').addClass("error-border-color");
+        $('#required-error-text2').text("未入力です");
+        // return false;
+    } else {
+        console.log("いいいいいいいいいい");
+    }
+
+    //ps-nameが空の時の処理
+    let valtest3 = $('input[name = "ps-name"]').val();
+
+    if (valtest3 === '') {
+        $('.input-error-border3').addClass("error-border-color");
+        $('#required-error-text3').text("未入力です");
+        // return false;
+    } else {
+        console.log("いいいいいいいいいい");
+    }
+
+    //sps-nameが空の時の処理
+    let valtest4 = $('input[name = "sps-name"]').val();
+
+    if (valtest4 === '') {
+        $('.input-error-border4').addClass("error-border-color");
+        $('#required-error-text4').text("再入力したパスワードが違います");
+        // return false;
     } else {
         console.log("いいいいいいいいいい");
     }
