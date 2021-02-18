e675c4f8a55eb82ad1323a5f68f0a7944f6668d1
鈴木　航也 <kouyan9614@gmail.com>
2021-02-19 07:52:54 +0900

    0219_新規登録画面_配列→変数1つvar

--- source/public/js/functions.js
+++ source/public/js/functions.js
@@ -6,56 +6,60 @@ $('#submitbtn').click(function () {
 
     //user-nameが空の時の処理
     let valtest1 = $('input[name = "user-name"]').val();
-    let status = [];
+    let status;
 
     if (valtest1 === '') {
         // Array.prototype.push.call(status, 1);
-        status.push("ng");
+        status = "ng";
         $('.input-error-border1').addClass("error-border-color");
         $('#required-error-text1').text("未入力です");
     } else {
-        status.push("ok");
-        console.log("送信完了しました");
+        $('.input-error-border1').removeClass("error-border-color");
+        $('#required-error-text1').text('');
+        console.log("入力okです1");
     }
 
     //email-nameが空の時の処理
     let valtest2 = $('input[name = "email-name"]').val();
 
     if (valtest2 === '') {
-        status.push("ng");
+        status = "ng";
         $('.input-error-border2').addClass("error-border-color");
         $('#required-error-text2').text("未入力です");
     } else {
-        status.push("ok");
-        console.log("送信完了しました");
+        $('.input-error-border2').removeClass("error-border-color");
+        $('#required-error-text2').text('');
+        console.log("入力okです2");
     }
 
     //ps-nameが空の時の処理
     let valtest3 = $('input[name = "ps-name"]').val();
 
     if (valtest3 === '') {
-        status.push("ng");
+        status = "ng";
         $('.input-error-border3').addClass("error-border-color");
         $('#required-error-text3').text("未入力です");
     } else {
-        status.push("ok");
-        console.log("送信完了しました");
+        $('.input-error-border3').removeClass("error-border-color");
+        $('#required-error-text3').text('');
+        console.log("入力okです3");
     }
 
     //sps-nameが空の時の処理
     let valtest4 = $('input[name = "sps-name"]').val();
 
     if (valtest4 === '') {
-        status.push("ng");
+        status = "ng";
         $('.input-error-border4').addClass("error-border-color");
         $('#required-error-text4').text("再入力したパスワードが違います");
     } else {
-        status.push("ok");
-        console.log("送信完了しました");
+        $('.input-error-border4').removeClass("error-border-color");
+        $('#required-error-text4').text('');
+        console.log("入力okです4");
     }
 
-    if (status.some(num => num === "ng")) {
-        console.log("未入力の箇所があります");
+    if (status === "ng") {
+        console.log("全体で未入力の箇所があります");
         return false;
     } else {
         console.log("正常に送信完了しました");
