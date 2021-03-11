47c7bca16e509fcac6ed304ff17ced1633a14858
mori <takaaki.578578@gmail.com>
2021-03-11 22:24:59 +0900

    セレクトボックス関数化

--- source/public/functions.php
+++ source/public/functions.php
@@ -7,4 +7,18 @@
     function filter($str){
       return filter_input(INPUT_POST, $str);
     }
+
+    // 新規投稿画面のセレクトボックス作成
+    function selectOption($tableName) {
+      
+      global $dbs;
+      $sql = 'select * from ' . $tableName;
+      $stmt = $dbs->query($sql);
+      $select_values = $stmt->fetchAll();
+      
+      foreach($select_values as $select_value) {
+        $select_tags .= '<option value="' . $select_value['cook_id'] .  '">' . $select_value['cook_name'] . '</option>';
+      }
+      return $select_tags;
+    }
 ?>
